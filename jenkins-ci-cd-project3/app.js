241a0f3f771ded9f93c2b5d14acb042b4ae6e843
Ubuntu <ubuntu@ip-172-31-33-112.ec2.internal>
2025-06-12 06:45:42 +0000

    Initial commit

--- jenkins-ci-cd-project3/app.js
+++ jenkins-ci-cd-project3/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js on Kubernetes!');
-});
+app.get('/', (req, res) => res.send('Hello from Kubernetes!'));
 
-app.listen(port, () => {
-  console.log(`App running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
