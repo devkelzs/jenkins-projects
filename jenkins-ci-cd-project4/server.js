977920cf148c94b53277d7368ac51db4293c11c7
Kelly Ankiambom <nkwainkelly8@gmail.com>
2025-06-13 10:36:45 +0100

    Update server.js

--- jenkins-ci-cd-project4/server.js
+++ jenkins-ci-cd-project4/server.js
@@ -2,7 +2,7 @@ const os = require('os');
 const app = require('./app');
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   const interfaces = os.networkInterfaces();
   console.log(`App is running on port ${port}`);
   console.log(`Local access: http://localhost:${port}`);
