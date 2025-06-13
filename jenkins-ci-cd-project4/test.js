8dced938e23fc8cd48f70d97b58ac092a75c76ec
Kelly Ankiambom <nkwainkelly8@gmail.com>
2025-06-13 09:42:39 +0100

    Update test.js

--- jenkins-ci-cd-project4/test.js
+++ jenkins-ci-cd-project4/test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app'); // adjust path if needed
 const assert = require('assert');
 
 describe('GET /', () => {
