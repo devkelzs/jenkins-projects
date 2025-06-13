e766a0b24c10d8fc28e60cc05823df57cca86a07
Ubuntu <ubuntu@ip-172-31-42-73.ec2.internal>
2025-06-13 07:02:32 +0000

    Initial commit

---
+++ jenkins-ci-cd-project4/test.js
@@ -0,0 +1,12 @@
+\const request = require('supertest');
+const app = require('../app');
+const assert = require('assert');
+
+describe('GET /', () => {
+  it('should return Hello World!', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Hello World!', done);
+  });
+});
